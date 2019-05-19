ab2e2af952ea2c8c54064ebc6de7515b65269d56
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-05-19 13:58:49 +1000

    add start+finish logs to bootstrap_agent.sh

--- .buildkite/bootstrap_agent.sh
+++ .buildkite/bootstrap_agent.sh
@@ -9,6 +9,8 @@
 #
 # Upload Cmd: aws --profile aws-2019-admin s3 cp --sse aws:kms .buildkite/bootstrap_agent.sh "s3://buildkite-agent-scripts/boostrap_agent.sh"
 
+echo "Starting Buildkite agent bootstrap..."
+
 
 BAZEL_VERSION="0.25.2"
 sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip
@@ -16,3 +18,6 @@ curl -sSL -O \
   "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
 chmod +x "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
 sudo "./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+
+
+echo "Finished Buildkite agent bootstrap..."
