578b368a5461ad182b5706e50adb57e42a13aa5a
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-05-19 16:27:14 +1000

    add 'python' prereq for Bazel

--- .buildkite/bootstrap_agent.sh
+++ .buildkite/bootstrap_agent.sh
@@ -13,7 +13,7 @@ echo "Starting Buildkite agent bootstrap..."
 
 
 BAZEL_VERSION="0.25.2"
-sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip
+sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip python
 curl -sSL -O \
   "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
 chmod +x "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
