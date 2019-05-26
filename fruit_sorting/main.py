b659c6aa1a2cfcb55717746a972861fb0ef04236
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-05-26 14:03:31 +1000

    Add very basic stub python program

---
+++ fruit_sorting/main.py
@@ -0,0 +1,15 @@
+import click
+
+
+@click.command()
+@click.option("--count", default=1, help="Number of greetings.")
+def cli(count):
+    """
+    Stub program. TODO(Jonathon): Implement a more interesting program
+    Example usage: bazel run //fruit_sorting:main -- --count 45
+    """
+    print("Hello world " * count)
+
+
+if __name__ == "__main__":
+    cli()
