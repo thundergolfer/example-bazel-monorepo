6a37da952d4bc0c6926a66877b1c8c5f82932046
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-09-29 14:55:39 +1000

    add script to run Bazel linter in CI

---
+++ tools/build/bazel_lint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+bazel run //:buildifier
+
+# TODO(Jonathon): Shouldn't buildifier support '--exclude' functionality?
+git checkout -- 3rdparty
+
+if [[ -z $(git status -s) ]]; then
+  echo "Need to run buildifier to lint BUILD files + WORKSPACE"
+  exit 1
+fi
