b681f2a9e34eb7d803e2e2a64fd5731634844c85
Jonathon Belotti <jonathon@canva.com>
2019-11-26 13:59:48 +1100

    Update Bazel version in CI

--- .buildkite/bootstrap_agent.sh
+++ .buildkite/bootstrap_agent.sh
@@ -13,7 +13,7 @@ echo "Starting Buildkite agent bootstrap..."
 
 # Can't upgrade to 0.25.2. Breaks rules_scala:
 # https://github.com/bazelbuild/bazel/issues/7498#issuecomment-489286392
-BAZEL_VERSION="0.24.1"
+BAZEL_VERSION="0.28.1"
 
 ## Install Bazel's and Project's dependencies
 
