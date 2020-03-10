a966f5d446f8c4f36832be7bc5fbbb041a678938
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-03-10 20:49:08 +1100

    update bazel-deps to use their advised update_dependencies.sh script https://github.com/johnynek/bazel-deps/releases

--- tools/update_jvm_dependencies.sh
+++ tools/update_jvm_dependencies.sh
@@ -1,18 +1,61 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -eux
+echo -ne "\033[0;32m"
+echo 'Updating bazel dependencies. This will take about five minutes.'
+echo -ne "\033[0m"
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ "$(uname -s)" == "Linux" ]; then
+  BAZEL_DEPS_URL="https://github.com/johnynek/bazel-deps/releases/download/v0.1-7/bazel-deps-linux"
+  BAZEL_DEPS_SHA256="8597279f17a4cc8d49169276a6266cf1ae342e5700b995c2bd3b83128d028d31"
+elif [ "$(uname -s)" == "Darwin" ]; then
+  BAZEL_DEPS_URL="https://github.com/johnynek/bazel-deps/releases/download/v0.1-7/bazel-deps-macos"
+  BAZEL_DEPS_SHA256="11e58b790566dbc23567faedef16d7f9177289b897fe790ffb8bbe057d06ebb8"
+else
+  echo "Your platform '$(uname -s)' is unsupported, sorry"
+  exit 1
+fi
+
 WORKSPACE_FILEPATH="./3rdparty/jvm_workspace.bzl"
 DEPS_YAML_FILEPATH="./tools/dependencies/jvm_dependencies.yaml"
 
-usage() { echo "./update_jvm_dependencies.sh <path to bazel-deps repo>"; }
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+SCRIPT_LOCATION="${REPO_ROOT}/tools/"
+cd "${SCRIPT_LOCATION}"
 
-if [[ -z "${1:-}" ]]; then
-    usage
-    exit 1
-fi
 
-cd ${1}
+BAZEL_DEPS_DIR="${HOME}/.bazel-deps-cache"
+BAZEL_DEPS_PATH="${BAZEL_DEPS_DIR}/v0.1-7"
 
-bazel run //:parse -- generate -r ${REPO_ROOT} -s ${WORKSPACE_FILEPATH} -d ${DEPS_YAML_FILEPATH}
+main() {
+  if [[ ! -f "${BAZEL_DEPS_PATH}" ]]; then
+      set -e
+      echo "Fetching bazel deps."
+      curl -L -o "/tmp/bazel-deps-bin" "${BAZEL_DEPS_URL}"
+
+      GENERATED_SHA_256="$(shasum -a 256 /tmp/bazel-deps-bin | awk '{print $1}')"
+
+      if [ "${GENERATED_SHA_256}" != "${BAZEL_DEPS_SHA256}" ]; then
+        echo "Sha 256 does not match, expected: ${BAZEL_DEPS_SHA256}"
+        echo "But found ${GENERATED_SHA_256}"
+        echo "You may need to update the sha in this script, or the download was corrupted."
+        exit 1
+      fi
+
+      chmod +x "/tmp/bazel-deps-bin"
+      mkdir -p "${BAZEL_DEPS_DIR}"
+      mv "/tmp/bazel-deps-bin" "${BAZEL_DEPS_PATH}"
+  fi
+
+  cd "${REPO_ROOT}"
+
+  $BAZEL_DEPS_PATH generate \
+    -r "${REPO_ROOT}" \
+    -s "${WORKSPACE_FILEPATH}" \
+    -d "${DEPS_YAML_FILEPATH}"
+}
+
+main "$@"
