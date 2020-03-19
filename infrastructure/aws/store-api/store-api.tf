dc48e858fe3fac5ab4e977edf3bff506757d6f5a
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-03-19 21:31:24 +1100

    move ami/ folder and add initial config for deploying store-api to ec2

---
+++ infrastructure/aws/store-api/store-api.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_version = "0.12.18"
+}
+
+variable "region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  foo = "bar"
+}
+
+data "aws_partition" "current" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "store_api" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "store-api" // Note: Created outside of Terraform using AWS Console
+
+  tags = {
+    Name = "store-api"
+  }
+}
