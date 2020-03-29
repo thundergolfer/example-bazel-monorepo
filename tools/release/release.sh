3523f6937b29d0edbbf14615ed57b994722603c3
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-03-29 21:16:37 +1100

    Try do s3 ls

--- tools/release/release.sh
+++ tools/release/release.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+ARTIFACTS_BUCKET="s3://example-bazel-monorepo-artifacts"
+
 main() {
   aws sts get-caller-identity
+
+  echo "${ARTIFACTS_BUCKET}/${BUILDKITE_COMMIT}"
+
+  aws s3 ls "${ARTIFACTS_BUCKET}"
 }
 
 main "$@"
