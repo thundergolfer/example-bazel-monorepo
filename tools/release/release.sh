af80b894ed49dc25721ad74f8883c2af23a6398a
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-03-29 21:38:15 +1100

    Try cp deploy jar to s3

--- tools/release/release.sh
+++ tools/release/release.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 ARTIFACTS_BUCKET="s3://example-bazel-monorepo-artifacts"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 main() {
+  local releaseable_targets
+
   aws sts get-caller-identity
 
   echo "${ARTIFACTS_BUCKET}/${BUILDKITE_COMMIT}"
 
   aws s3 ls "${ARTIFACTS_BUCKET}"
+
+  # shellcheck disable=SC2046
+  bazel build $(bazel query "attr(tags, release, //...)")
+
+  aws s3 cp "${REPO_ROOT}/bazel-bin/store-api/src/main/java/com/book/store/api/deployable.jar" "${ARTIFACTS_BUCKET}/${BUILDKITE_COMMIT}/store-api/src/main/java/com/book/store/api/deployable.jar"
 }
 
 main "$@"
