b997e12e067d50db339f3cf6464fc219aec7a0cc
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2021-05-30 15:27:23 +1000

    update packer conf to HCL from JSON and update bazel version used in CI

--- infrastructure/aws/ami/continuous_integration/prepare.sh
+++ infrastructure/aws/ami/continuous_integration/prepare.sh
@@ -2,27 +2,31 @@
 
 set -o errexit
 set -o pipefail
+set -o nounset
 
-BAZEL_VERSION="3.3.0"
+## Install Bazel and the other project's dependencies
+main() {
+  local bazel_version
+  bazel_version="${BAZEL_VERSION}"
 
-## Install Bazel's and Project's dependencies
+  # NOTE: Amazon Linux 2 uses 'yum' not 'apt-get'
+  sudo yum groupinstall -y "Development Tools"
+  sudo yum install -y zip unzip
+  # Copied from https://github.com/buildkite/elastic-ci-stack-for-aws/pull/583/files
+  sudo yum install -y python3-pip python3 python3-setuptools
+  sudo yum install -y java-1.8.0-openjdk-devel
 
-# NOTE: Amazon Linux 2 uses 'yum' not 'apt-get'
-sudo yum groupinstall -y "Development Tools"
-sudo yum install -y zip unzip
-# Copied from https://github.com/buildkite/elastic-ci-stack-for-aws/pull/583/files
-sudo yum install -y python3-pip python3 python3-setuptools
-sudo yum install -y java-1.8.0-openjdk-devel
+  # Install Node
+  curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh" | bash
+  # shellcheck disable=SC1090
+  . ~/.nvm/nvm.sh
+  nvm install node
 
-# Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# shellcheck disable=SC1090
-. ~/.nvm/nvm.sh
-nvm install node
+  ## Install Bazel from source
+  curl -sSL -O \
+    "https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-installer-linux-x86_64.sh"
+  chmod +x "bazel-${bazel_version}-installer-linux-x86_64.sh"
+  sudo "./bazel-${bazel_version}-installer-linux-x86_64.sh"
+}
 
-## Install Bazel from source
-
-curl -sSL -O \
-  "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
-chmod +x "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
-sudo "./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+main "$@"
--- infrastructure/aws/ami/continuous_integration/rebuild.sh
+++ infrastructure/aws/ami/continuous_integration/rebuild.sh
@@ -4,7 +4,23 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-packer build \
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+main() {
+  local bazel_version
+  local bazelversion_dotfile_path
+
+  bazelversion_dotfile_path="${REPO_ROOT}/.bazelversion"
+  bazel_version="$(tr -d '\n' < "${bazelversion_dotfile_path}")"
+
+  cd "${REPO_ROOT}/infrastructure/aws/ami/continuous_integration/"
+
+  packer build \
     -var "aws_access_key=${AWS_ACCESS_KEY_ID}" \
     -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" \
-    "packer-conf.json"
+    -var "bazel_version=${bazel_version}" \
+    "./packer-conf.pkr.hcl"
+}
+
+
+main "$@"
