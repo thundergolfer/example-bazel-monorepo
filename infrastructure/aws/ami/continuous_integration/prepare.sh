273c7154c228e6b144741df66456e1c27dbfd591
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2021-06-26 16:35:45 +1000

    updates to AMI prep script so that new py toolchain works in CI

--- infrastructure/aws/ami/continuous_integration/prepare.sh
+++ infrastructure/aws/ami/continuous_integration/prepare.sh
@@ -12,7 +12,9 @@ main() {
   # NOTE: Amazon Linux 2 uses 'yum' not 'apt-get'
   sudo yum groupinstall -y "Development Tools"
   sudo yum install -y zip unzip
-  # Copied from https://github.com/buildkite/elastic-ci-stack-for-aws/pull/583/files
+
+  sudo yum install -y zstd
+  # TODO(Jonathon): Can remove python packages when 'black' is no longer pip installed in bootstrap script.
   sudo yum install -y python3-pip python3 python3-setuptools
   sudo yum install -y java-1.8.0-openjdk-devel
 
@@ -22,7 +24,7 @@ main() {
   . ~/.nvm/nvm.sh
   nvm install node
 
-  ## Install Bazel from source
+  ## Install Bazel
   curl -sSL -O \
     "https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-installer-linux-x86_64.sh"
   chmod +x "bazel-${bazel_version}-installer-linux-x86_64.sh"
